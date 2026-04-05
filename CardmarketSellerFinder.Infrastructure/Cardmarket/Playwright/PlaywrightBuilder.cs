8acf6555a544d64dd2f5fb634b8b819583caf174
Evil Mandroso <pol.puigdomenech.r@gmail.com>
2026-04-05 18:10:01 +0200

    playwright session builder

---
+++ CardmarketSellerFinder.Infrastructure/Cardmarket/Playwright/PlaywrightBuilder.cs
@@ -0,0 +1,34 @@
+﻿using CMBuyerStudio.Infrastructure.Cardmarket.Playwright;
+using Microsoft.Playwright;
+
+namespace CMBuyerStudio.Infrastructure.Cardmarket.Builders
+{
+    public class PlaywrightBuilder
+    {
+        public async Task<PlaywrightSession> CreateChromiumAsync(bool headless = false, Proxy? proxy = null)
+        {
+            var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = true,
+                Proxy = proxy
+            });
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+            return new PlaywrightSession(playwright, browser, context, page);
+        }
+
+        public async Task<PlaywrightSession> CreateFirefoxAsync(bool headless = false, Proxy? proxy = null)
+        {
+            var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+            var browser = await playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = true,
+                Proxy = proxy
+            });
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+            return new PlaywrightSession(playwright, browser, context, page);
+        }
+    }
+}
---
+++ CardmarketSellerFinder.Infrastructure/Cardmarket/Playwright/PlaywrightSession.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Playwright;
+
+namespace CMBuyerStudio.Infrastructure.Cardmarket.Playwright
+{
+    public sealed class PlaywrightSession : IAsyncDisposable
+    {
+        public IPlaywright Playwright { get; }
+        public IBrowser Browser { get; }
+        public IBrowserContext Context { get; }
+        public IPage Page { get; }
+
+        public PlaywrightSession(
+            IPlaywright playwright,
+            IBrowser browser,
+            IBrowserContext context,
+            IPage page)
+        {
+            Playwright = playwright;
+            Browser = browser;
+            Context = context;
+            Page = page;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await Context.DisposeAsync();
+            await Browser.DisposeAsync();
+            Playwright.Dispose();
+        }
+    }
+}
